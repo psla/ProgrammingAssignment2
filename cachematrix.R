1910c3e6fd9c299f0cb1afaa25fe93d1bbe921b5
Peter Slatala <pslatala@microsoft.com>
2014-05-21 21:33:28 -0700

    Alternative solution proposal

--- cachematrix.R
+++ cachematrix.R
@@ -54,6 +54,63 @@ cacheSolve <- function(x, ...) {
   # store computed value in cache
   x$setinverse(computedInverse)
   
-  # return the value
+  # return the computed value
   computedInverse
+}
+
+## THIS IS THE END OF ASSIGNMENT; READ BELOW IF YOU WANT TO GIVE ME PERSONAL FEEDBACK ON DIFFERENT APPROACH ##
+## (you can shoot me an email luserek at gmail)
+
+
+### 
+### I do not think that suggested proposal of methods is a good one
+### The problem I see is that makeCacheMatrix returns a public setters for the cache item
+### and everyone who sees this object can change it - even when it won't be good thing to do
+### This breaks encapsulation of fields.
+### I think that cacheSolve should be part of the makeCacheMatrix and setinverse should not be exposed;
+### getinverse should call inverse and cache it (given that the cache already belongs to the matrix)
+###
+### See the example below:
+
+# Example:
+# cm <- cachedMatrix(hilbert(8))
+# cm$getinverse() # will compute it
+# cm$getinverse() # will return cached value
+cachedMatrix <- function(x = matrix()) {
+  # initialize the variable which will store the inversed cached value
+  # initially do not assign any value (it will be lazily assigned when cacheSolve is called)
+  inversedCache <- NULL
+  
+  # returns the backend matrix
+  get <- function() x
+  
+  # changes the stored matrix to a different one
+  # cleares cache
+  set <- function(newMatrix) {
+    x <<- newMatrix
+    inversedCache <<- NULL
+  }
+  
+  # this function now computes inverse if it is yet computed or returns cached value
+  # The advantage of this approach is that nobody should be able to change cached value ever; only this method
+  # can do it; which guarantees that it corresponds correctly to the matrix itself
+  # the logic in this function is the same as the logic in the cacheSolve above, but it is encapsulated
+  getinverse <- function() {
+    # same logic as earlier: see if we already have cached value, if not, compute and cache it
+    if(!is.null(inversedCache)) {
+      message("getting cached data")
+      return(inversedCache)
+    }
+    
+    # compute
+    inversedCache <<- solve(x)
+    
+    # return computed value
+    inversedCache
+  }
+  
+  # returns a 'public API' of this makeCacheMatrix
+  # the public API now does not expose setinverse as it is handled internally
+  list(set = set, get = get,
+       getinverse = getinverse)
 }
